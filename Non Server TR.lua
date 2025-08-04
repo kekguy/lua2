c575cf7f4bd53924991771a60bd2dfadfc207951
kekguy <31189762+kekguy@users.noreply.github.com>
2025-08-04 10:40:40 +0100

    Update Non Server TR.lua
    ttemp

--- Non Server TR.lua
+++ Non Server TR.lua
@@ -35,8 +35,8 @@ spawn(function()
     getgenv().PetHatcher = {
         Webhook = "https://discord.com/api/webhooks/1368315112737210398/ydQykT7Wt2IqZy94QkmVXOHkgE5jvPw0mYNSiUNJBn2ClRlk0-M7GFLcD6IjwsnWwLGU",
         Ping = "<@&785853304328421447> Secret Hatched!",
-        Rarities = {"Legendary", "Secret", "Infinity"},
-        Ignore = {"Unicorn", "Gummy Dragon", "Angel Bee", "Space Invader", "Neon Wire Eye", "Virus", "Hexarium", "Emerald Golem", "Rock Candy Golem", "Void Eye", "Stone Gargoyle", "Neon Wyvern", "Angel", "Neon Elemental", "Lunar Serpent", "Electra", "Inferno Dragon", "Cyborg Phoenix", "Crystal Unicorn", "Green Hydra", "Inferno Cube", "Dark Phoenix", "Flying Pig"},
+        Rarities = {"Secret", "Infinity"},
+        Ignore = {"Mech Robot", "Unicorn", "Gummy Dragon", "Angel Bee", "Space Invader", "Neon Wire Eye", "Virus", "Hexarium", "Emerald Golem", "Rock Candy Golem", "Void Eye", "Stone Gargoyle", "Neon Wyvern", "Angel", "Neon Elemental", "Lunar Serpent", "Electra", "Inferno Dragon", "Cyborg Phoenix", "Crystal Unicorn", "Green Hydra", "Inferno Cube", "Dark Phoenix", "Flying Pig"},
         SkipEasyLegends = true,
         HatchAmounts = false,
         Server = false,

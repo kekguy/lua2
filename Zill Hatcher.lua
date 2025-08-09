77c0aa04d505267c18ed4dee6a29e42f0ec13c1d
kekguy <31189762+kekguy@users.noreply.github.com>
2025-08-09 18:39:56 +0100

    Update Zill Hatcher.lua

--- Zill Hatcher.lua
+++ Zill Hatcher.lua
@@ -36,7 +36,7 @@ spawn(function()
         Webhook = "https://discord.com/api/webhooks/1368349602901921884/KBMcZ3469kfFuypOdovJWoYd3ghB46rGesPi2BBR7Pn9JbyYCooaOyqjg0i6kIzMAiHV",
         Ping = "<@807429851895300107> hey nigger nice secret",
         Rarities = {"Legendary", "Secret", "Infinity"},
-        Ignore = {"Unicorn", "Gummy Dragon", "Angel Bee", "Space Invader", "Neon Wire Eye", "Virus", "Hexarium", "Emerald Golem", "Minty Serpent", "Magma Cube", "Thorn Dragon", "Void Eye", "Stone Gargoyle", "Neon Wyvern", "Angel", "Neon Elemental", "Lunar Serpent", "Electra", "Inferno Dragon", "Cyborg Phoenix", "Crystal Unicorn", "Green Hydra", "Inferno Cube", "Dark Phoenix", "Flying Pig"},
+        Ignore = {"Unicorn", "Marshmallow", "Angel Bee", "Space Invader", "Neon Wire Eye", "Virus", "Hexarium", "Emerald Golem", "Minty Serpent", "Magma Cube", "Thorn Dragon", "Void Eye", "Stone Gargoyle", "Neon Wyvern", "Angel", "Neon Elemental", "Lunar Serpent", "Electra", "Inferno Dragon", "Cyborg Phoenix", "Crystal Unicorn", "Green Hydra", "Inferno Cube", "Dark Phoenix", "Flying Pig"},
         SkipEasyLegends = true,
         HatchAmounts = false,
         Server = false,
